6e281adcc0abeb4caba76846c0cdc75539a18bbf
tobiasmullins <tobias.mullins@student.mendocino.edu>
2025-07-22 16:05:37 -0700

    Update player_ver2.py
    Added a line to __init__ function , self.onBoard = False, to fix an error. Cleaned up code.

--- player_ver2.py
+++ player_ver2.py
@@ -1,30 +1,27 @@
-# In file: player.py
+# player.py
+# Tobias Mullins
+# CSC-220: Final Project
+# July 22, 2025
+
+
 from die import Die
 
 class Player:
-    """Represents a player, managing their dice, score, and game state."""
-
+    
     def __init__(self, name):
         self.name = name
         self.totalScore = 0
-        self.dice = []       # Dice just rolled, available to be held
-        self.heldDice = []   # Dice kept for points this turn
+        self.dice = []       
+        self.heldDice = []  
+        self.onBoard = False
+        
 
     def rollDice(self):
-        """Rolls the dice that are not currently being held."""
         numToRoll = 6 - len(self.heldDice)
         self.dice = [Die() for _ in range(numToRoll)]
 
     def hold(self, diceValuesToKeep):
-        """
-        Moves dice from the active roll to the held list based on their values.
-        
-        Args:
-            diceValuesToKeep (list): A list of integer values of dice to keep.
-        """
         keptDieObjects = []
-        
-        # For each value we need to keep, find a matching Die object
         for value in diceValuesToKeep:
             for dieObject in self.dice:
                 if dieObject.getValue() == value:
@@ -34,22 +31,17 @@ class Player:
         
         self.heldDice.extend(keptDieObjects)
 
-
     def resetDice(self):
-        """Resets the player's hand for the next turn."""
         self.dice = []
         self.heldDice = []
 
     def addToScore(self, points):
-        """Adds points to the player's total score."""
         self.totalScore += points
 
     def getTotalScore(self):
-        """Returns the player's total score."""
         return self.totalScore
 
     def isOnBoard(self, turnTotal):
-        """Checks if the player is "on the board"."""
         if self.onBoard:
             return True
         if turnTotal >= 1000:
