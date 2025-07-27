6c172febbeb3decf15e2cd39c8b3f4ed066d1868
tobiasmullins <tobias.mullins@student.mendocino.edu>
2025-07-26 20:33:23 -0700

    Update main.py
    Added Comments

--- main.py
+++ main.py
@@ -6,7 +6,9 @@
 from player import Player
 from game import Game
 
+# This function sets up and starts the game.
 def main():
+    # Loop to get a valid number of players from the user.
     while True:
         try:
             numStr = input("Enter the number of players: ")
@@ -16,8 +18,9 @@ def main():
             else:
                 print("Please enter a number between 1 and 6.")
         except ValueError:
-            print("Invalid input. Please enter a number")
+            print("Invalid input. Please enter a number.")
 
+    # Create a list of Player objects based on user input.
     playerList = []
     for i in range(num):
         while True:
@@ -29,10 +32,12 @@ def main():
                 print("Name cannot be empty. Please enter a name.")
 
     print()
-    print("Let's play 10,000!\n")
+    print("Let's play Zilch (10,000)!\n")
 
+    # Create a new game instance and start the game.
     game = Game(playerList)
     game.play()
 
+# This ensures the main function runs only when the script is executed directly.
 if __name__ == "__main__":
     main()
